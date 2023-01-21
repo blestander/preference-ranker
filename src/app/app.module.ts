5ffb817f88bb747e85496ebb15ac033495990923
Brendan Lestander <blestander@gmail.com>
2023-01-21 14:56:59 -0500

    Added validation to option entry.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { OptionEntryComponent } from './option-entry/option-entry.component';
@@ -17,6 +18,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
+
+        ReactiveFormsModule,
 
         MatButtonModule,
         MatFormFieldModule,
--- src/app/option-entry/option-entry.component.ts
+++ src/app/option-entry/option-entry.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
     selector: 'app-option-entry',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class OptionEntryComponent {
 
+    formControl = new FormControl("");
+
+    constructor (private fb: FormBuilder) {
+        this.formControl.addValidators((control) => {
+            const value = control.value as string;
+            const lines = value
+                .split('\n')
+                .map(line => line.trim())
+                .filter(line => line.length > 0);
+            if (lines.length >= 2)
+                return null;
+            else
+                return { notEnoughLines: true };
+        })
+    }
+
+    get shouldDisableButton(): boolean {
+        return this.formControl.invalid;
+    }
 }
