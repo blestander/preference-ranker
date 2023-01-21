f2e3de90ef1121187d5a3832176c6b7d00ef4005
Brendan Lestander <blestander@gmail.com>
2023-01-21 14:14:53 -0500

    Created basic field for entering options.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { OptionEntryComponent } from './option-entry/option-entry.component';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
     declarations: [
         AppComponent,
         OptionEntryComponent
     ],
     imports: [
-        BrowserModule
+        BrowserModule,
+        BrowserAnimationsModule,
+
+        MatFormFieldModule,
+        MatInputModule,
     ],
     providers: [],
     bootstrap: [AppComponent]
